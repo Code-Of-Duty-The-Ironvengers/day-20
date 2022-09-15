6717b4f55c40c51832f0ab999d1bbe9489922a5f
André de Albuquerque <itstheandre@gmail.com>
2022-09-15 20:08:23 +0200

    Done

--- props/src/App.js
+++ props/src/App.js
@@ -4,6 +4,33 @@ import iCanMakeOutside from "./call-whatever";
 
 // document.qouerySelector('a').style.background = 'red'
 
+function EitherMaximeJokeOrSomethingElse(props) {
+  console.log("props:", props);
+  // if we are passing to this compoennt a makeJoke prop, display a joke about maxime lying abour her age
+  // otherwise just should a div saying todays dateo
+  if (props.makeJoke) {
+    return (
+      <div>
+        Make a joke about how much we all have fun making fun of maxime lying
+        about her age
+        <h1>ITS BEEN 4 months and she's still lying</h1>
+        <h2>
+          And look at her, shes laughing at us, because she doesnt want to admit
+          her age
+        </h2>
+        <h3>Its almost as though she wants us to suffer</h3>
+        <h4>Almost as much as she did when she took the studies</h4>
+        <h5>
+          Even though, you can make the argument she loved the studies masters
+          so much, she took it twice
+        </h5>
+      </div>
+    );
+  }
+
+  return <div>Today is the 15th of September</div>;
+}
+
 function App() {
   const hello = "hello there, daiane is feeling. hmmmm this is suspicious";
 
@@ -12,11 +39,15 @@ function App() {
   }
   return (
     <div className="App">
-      {hello}
-      <Button backgroundColor="blue" />
-      <Button />
-      <Button backgroundColor="green" asemAskedAQuestion="yes" />
-      <Swag
+      <EitherMaximeJokeOrSomethingElse />
+      <div style={{ height: "50vh" }}></div>
+      {/* {hello} */}
+      <Button text="Hello" backgroundColor="blue" />
+      <Button>
+        <h2>INSIDE OF A BUTTON</h2>
+      </Button>
+      <Button text="Hello" backgroundColor="green" asemAskedAQuestion="yes" />
+      {/* <Swag
         maximesAge={12}
         maximesMasters={["studies", "art history", "studies again"]}
         // isMaximeLyingAboutHerAge={false}
@@ -32,8 +63,8 @@ function App() {
         makesNoSense={NaN}
         fancy={Button}
         fromOutside={iCanMakeOutside}
-      />
-      <header className="App-header">
+      /> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -46,7 +77,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
@@ -60,10 +91,16 @@ function Swag(props) {
 }
 
 function Button(props) {
+  console.log("props:", props);
   const { backgroundColor = "red" } = props;
   return (
     <div>
-      <button style={{ backgroundColor }}>Hello</button>
+      <button
+        onClick={() => console.log("Clicked")}
+        style={{ backgroundColor }}
+      >
+        {props.children || "Hello"}
+      </button>
     </div>
   );
 }
